68c80dc13440f7354bc90e374d0f6602f24571e0
Ridho Anandamal <a.ridho00@gmail.com>
2022-10-08 21:43:38 +0700

    [Update] Render list based on local data and add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import PageHome from "@/components/PageHome.vue";
+</script>
+
 <template>
-  <div></div>
+  <div class="container">
+    <PageHome />
+  </div>
 </template>
+
+<style lang="scss">
+@import "@/assets/style.css";
+@import "~bootstrap/dist/css/bootstrap.css";
+</style>
