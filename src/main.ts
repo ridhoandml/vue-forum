94792ac9b128792def3178aba4736db99da7fe78
Ridho Anandamal <a.ridho00@gmail.com>
2022-10-08 20:07:01 +0700

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const forumApp = createApp(App);
+
+// forumApp.component("NiceButton", {});
+// forumApp.use(SomePlugin);
+forumApp.mount("#app");
